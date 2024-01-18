dd1f0d829d83de2fd66e2d16ec2791692997fd3f
Orinda Harrison(Mitch) <mitchke@duck.com>
2024-01-18 20:57:03 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,6 @@
 
 cpu_count=$(grep -c processor /proc/cpuinfo)
 # https://docs.gunicorn.org/en/latest/design.html#how-many-workers
-exec python manage.py collectstatic --noinput --verbosity 0 && python manage.py migrate --noinput && gunicorn -b :"${PORT:-8000}" --reload interintel.wsgi:application
+python manage.py collectstatic --noinput --verbosity 0
+python manage.py migrate --noinput
+exec gunicorn -b :"${PORT:-8000}" --reload interintel.wsgi:application
